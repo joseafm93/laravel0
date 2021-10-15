9bf9dafcd06855ca9bb597175aaee08c685a143f
root <joseafm93@gmail.com>
2021-10-15 18:31:20 +0000

    MCELGPHDPTMP

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,26 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('usuarios', function(){
+    return '<h1>Usuarios</h1>';
+});
+
+// ESTO NO SE USA
+// Route::get('usuarios/detalles', function() {
+//     return 'mostrando detalles del usuario: ' . $_GET['id'];
+// });
+
+Route::get('usuarios/nuevo', function() {
+    return 'Creando un nuevo usuario';
+});
+
+Route::get('usuarios/{id}', function($id) {
+    return 'mostrando detalles del usuario: ' . $id;
+}) -> where('id', '[0-9]+');
+
+Route::get('saludo/{name}/{nickname?}', function($name, $nickname = null) {
+    return $nickname
+    ? 'Bienvenido ' . $name . '. Tu apodo es ' . $nickname
+    : 'Bienvenido ' . $name . '.';
+});
