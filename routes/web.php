b98be469193dfde2b199104c16673c2ae4a45ea5
root <joseafm93@gmail.com>
2021-10-19 13:57:27 +0000

    Controladores hechos

--- routes/web.php
+++ routes/web.php
@@ -15,25 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('usuarios', function(){
-    return '<h1>Usuarios</h1>';
-});
+Route::get('usuarios', 'UserController@index');
+Route::get('usuarios/nuevo', 'UserController@create');
+Route::get('usuarios/{id}', 'UserController@show')->where('id', '[0-9]+');
 
-// ESTO NO SE USA
-// Route::get('usuarios/detalles', function() {
-//     return 'mostrando detalles del usuario: ' . $_GET['id'];
-// });
-
-Route::get('usuarios/nuevo', function() {
-    return 'Creando un nuevo usuario';
-});
-
-Route::get('usuarios/{id}', function($id) {
-    return 'Mostrando detalles del usuario: ' . $id;
-}) -> where('id', '[0-9]+');
-
-Route::get('saludo/{name}/{nickname?}', function($name, $nickname = null) {
-    return $nickname
-    ? 'Bienvenido ' . ucfirst($name) . '. Tu apodo es ' . $nickname
-    : 'Bienvenido ' . ucfirst($name) . '.';
-});
+Route::get('saludo/{name}/{nickname?}', 'WelcomeUserController');
