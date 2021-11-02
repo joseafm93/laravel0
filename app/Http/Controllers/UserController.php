c8cf5e95229b386f79553c1eee36e4300d649f15
joseafm93 <joseafm93@gmail.com>
2021-11-02 14:37:54 +0000

    Paso 34 completo

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -67,7 +67,7 @@ class UserController extends Controller
         $data = request()->validate([
             'name' => 'required',
             'email' => 'required|email|unique:users,email',
-            'password' => '',
+            'password' => 'required',
         ]);
 
         $data['password'] = bcrypt($data['password']);
