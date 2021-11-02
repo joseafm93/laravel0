7fcc70b96ce806aaf50d827262d0993a750c879f
joseafm93 <joseafm93@gmail.com>
2021-11-02 14:51:49 +0000

    Paso 35

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,7 +29,7 @@ class UserController extends Controller
     {
         $data = $request->validate([
             'name' => 'required',
-            'email' => 'required|email|unique:users,email',
+            'email' => 'required|email',
             'password' => 'required',
 
         ], [
@@ -67,10 +67,14 @@ class UserController extends Controller
         $data = request()->validate([
             'name' => 'required',
             'email' => 'required|email|unique:users,email',
-            'password' => 'required',
+            'password' => '',
         ]);
 
-        $data['password'] = bcrypt($data['password']);
+        if($data['password'] != null) {
+            $data['password'] = bcrypt($data['password']);
+        } else {
+            unset($data['password']);
+        }
 
         $user->update($data);
 
